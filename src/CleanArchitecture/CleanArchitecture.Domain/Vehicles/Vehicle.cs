7264637234e5e2e0f499e9bec18ef66373adb297
hgamboa <hgamboa@polpulsa.com>
2025-01-21 17:54:21 -0600

    Se termino el modelo vehicle

--- src/CleanArchitecture/CleanArchitecture.Domain/Vehicles/Vehicle.cs
+++ src/CleanArchitecture/CleanArchitecture.Domain/Vehicles/Vehicle.cs
@@ -9,13 +9,30 @@ namespace CleanArchitecture.Domain.Vehiculos
 {
     public sealed class Vehiculo:Entity
     {
-        public Vehiculo (Guid id):base(id)
-        {}
+        public Vehiculo (
+            Guid id,
+            Model model,
+            Vin vin,
+            Currency currencyPrice,
+            Currency currencyMaintenance,
+            DateTime ?dateOfLastRental,
+            Addresses addresses,
+            List<Accessory> accessories
+            ):base(id)
+        {
+            Model = model;
+            Vin = vin;
+            Price = currencyPrice;
+            MaintenanceCurrency = currencyMaintenance;
+            DateOfLastRental = dateOfLastRental ?? DateTime.MinValue;
+            Addresses = addresses;
+            Accessories = accessories;
+        }
         public Model ?Model { get; private set; }
         public Vin ?Vin { get; private set; }
         public Addresses ?Addresses { get; private set; }
-        public Currency Price{ get; private set; }
-        public Currency MaintenanceCurrency{ get; private set; }
+        public Currency? Price{ get; private set; }
+        public Currency? MaintenanceCurrency{ get; private set; }
         public DateTime DateOfLastRental  { get; private set; }
         public List<Accessory> Accessories { get; private set; } = [];
     }
